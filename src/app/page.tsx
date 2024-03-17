9e2429e5285d33964db67abf5f63b49e7dc36e4f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-17 14:33:15 +0200

    Update logo to be reusable & small by default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { SignInButton } from '@/components/auth/SignInButton';
 const Home = () => (
   <main className='flex h-full flex-col items-center justify-center'>
     <div className='space-y-6 text-center'>
-      <Logo />
+      <Logo className='text-6xl text-white drop-shadow-md' />
       <SignInButton>
         <Button
           variant='secondary'
--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,15 +1,31 @@
+import {
+  forwardRef,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+} from 'react';
 import { LockClosedIcon } from '@radix-ui/react-icons';
 
-export const Logo = () => (
+import { cn } from '@/lib/utils';
+
+export const Logo = forwardRef<
+  ElementRef<'h1'>,
+  ComponentPropsWithoutRef<'h1'>
+>(({ className, ...props }, ref) => (
   <h1
-    className='flex select-none flex-row items-center text-6xl
-font-extrabold tracking-tight text-white drop-shadow-md'
+    {...props}
+    ref={ref}
+    className={cn(
+      `flex select-none flex-row items-center text-2xl font-extrabold
+tracking-tight text-primary`,
+      className
+    )}
   >
-    <LockClosedIcon className='size-48' />
-    <div className='flex flex-col items-start'>
+    <LockClosedIcon className='size-[3.25em]' />
+    <div className='flex flex-col items-start leading-none'>
       <span>The</span>
       <span>Auth</span>
       <span>Thing</span>
     </div>
   </h1>
-);
+));
+Logo.displayName = 'Logo';
