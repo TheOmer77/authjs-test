69abf5e2b4d5bd8203dd77c43b9496540fa5a820
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-17 18:59:02 +0200

    Create auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,8 +1,14 @@
 import type { PropsWithChildren } from 'react';
 
+import { Card } from '@/components/ui/Card';
+import { AppLogo } from '@/components/layout/logos';
+
 const AuthLayout = ({ children }: PropsWithChildren) => (
   <div className='flex h-full flex-col items-center justify-center'>
-    {children}
+    <Card className='w-full max-w-md'>
+      <AppLogo className='p-6 pb-0' />
+      {children}
+    </Card>
   </div>
 );
 
--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,21 +1,14 @@
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/Card';
+import { CardDescription, CardHeader, CardTitle } from '@/components/ui/Card';
 import { SignInForm } from '@/components/auth/SignInForm';
-import { AppLogo } from '@/components/layout/logos';
 
 const SignInPage = () => (
-  <Card className='w-full max-w-md'>
+  <>
     <CardHeader>
-      <AppLogo className='mb-4' />
       <CardTitle>Welcome back!</CardTitle>
       <CardDescription>Enter your account details to continue.</CardDescription>
     </CardHeader>
     <SignInForm />
-  </Card>
+  </>
 );
 
 export default SignInPage;
