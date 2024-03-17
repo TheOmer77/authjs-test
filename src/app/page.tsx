df8f8a885dcc96de285145ac8ffb3c6864eb905a
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-17 13:28:57 +0200

    Update page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-const Home = () => <>Hello world.</>;
+const Home = () => (
+  <main
+    className='flex h-full flex-col items-center justify-center bg-background
+bg-[radial-gradient(circle_at_top,var(--tw-gradient-stops))] from-primary/30
+to-primary dark:from-primary/70 dark:to-primary/10'
+  >
+    Hello world.
+  </main>
+);
 
 export default Home;
