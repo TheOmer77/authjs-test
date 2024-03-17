073435f0289a52b0810cee025d819e387deac660
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-17 18:59:16 +0200

    Add signUp schema

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -10,3 +10,12 @@ export const signInSchema = z.object({
   password: z.string().min(1, { message: 'Password is required.' }),
 });
 export type SignInValues = z.infer<typeof signInSchema>;
+
+export const signUpSchema = z.object({
+  email,
+  password: z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters long.' }),
+  name: z.string().min(1, { message: 'Name is required.' }),
+});
+export type SignUpValues = z.infer<typeof signUpSchema>;
