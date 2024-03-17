50094fe918a276590d36cefa8fe390e86103be2b
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-17 20:58:11 +0200

    Implement user creation

--- src/actions/signUp.ts
+++ src/actions/signUp.ts
@@ -1,6 +1,9 @@
 'use server';
 
+import bcrypt from 'bcrypt';
+
 import { signUpSchema, type SignUpValues } from '@/schemas/auth';
+import { db } from '@/lib/db';
 
 export const signUp = async (
   values: SignUpValues
@@ -8,6 +11,19 @@ export const signUp = async (
   const validatedValues = signUpSchema.safeParse(values);
   if (!validatedValues.success)
     return { success: false, error: validatedValues.error.message };
+
+  const { email, name, password } = validatedValues.data;
+
+  const existingUser = (await db.user.count({ where: { email } })) > 0;
+  if (existingUser)
+    return { success: false, error: 'A user with this email already exists.' };
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await db.user.create({
+    data: { email, name, password: hashedPassword },
+  });
+
+  // TODO: Send verification token email
 
   return { success: true };
 };
