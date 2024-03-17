7c69bcb0a4991b4a23160b70d25539b15da9d558
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-17 13:55:45 +0200

    Add SignInButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/Button';
 import { Logo } from '@/components/layout/Logo';
+import { SignInButton } from '@/components/auth/SignInButton';
 
 const Home = () => (
   <main
@@ -6,8 +8,17 @@ const Home = () => (
 bg-[radial-gradient(circle_at_top,var(--tw-gradient-stops))] from-primary/30
 to-primary dark:from-primary/70 dark:to-primary/10'
   >
-    <div className='space-6'>
+    <div className='space-y-6 text-center'>
       <Logo />
+      <SignInButton>
+        <Button
+          variant='secondary'
+          size='lg'
+          className='bg-background text-foreground'
+        >
+          Sign in
+        </Button>
+      </SignInButton>
     </div>
   </main>
 );
---
+++ src/components/auth/SignInButton.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import type { PropsWithChildren } from 'react';
+import { useRouter } from 'next/navigation';
+import { Slot } from '@radix-ui/react-slot';
+
+export type LoginButtonProps = PropsWithChildren<{
+  mode?: 'modal' | 'redirect';
+}>;
+
+export const SignInButton = ({
+  mode = 'redirect',
+  children,
+}: LoginButtonProps) => {
+  const router = useRouter();
+
+  // TODO: Implement modal & remove this error
+  if (mode === 'modal')
+    throw new Error('LoginButton modal not implemented yet.');
+
+  const onClick = () => {
+    router.push('/auth/login');
+  };
+
+  return <Slot onClick={onClick}>{children}</Slot>;
+};
