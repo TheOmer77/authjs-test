9747d8c6fb8643b1c56406166f6640646b591f9a
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-17 13:29:49 +0200

    Change font to Manrope

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,6 +51,9 @@ const config = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      fontFamily: {
+        sans: 'var(--font-sans)',
+      },
       keyframes: {
         'accordion-down': {
           from: { height: '0' },
