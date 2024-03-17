9822730bf3f453f454feca413a327e3b749f701a
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-17 16:29:06 +0200

    Update SocialButtons component

--- src/components/auth/SocialButtons.tsx
+++ src/components/auth/SocialButtons.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import type { ComponentPropsWithoutRef } from 'react';
+
 import { Button } from '@/components/ui/Button';
 import { GithubLogo, GoogleLogo } from '@/components/layout/logos';
 
-export type SocialButtonsProps = {
+export type SocialButtonsProps = ComponentPropsWithoutRef<'button'> & {
   onGoogleClick?: () => void;
   onGithubClick?: () => void;
 };
@@ -11,16 +13,32 @@ export type SocialButtonsProps = {
 export const SocialButtons = ({
   onGithubClick,
   onGoogleClick,
+  ...props
 }: SocialButtonsProps) => (
-  <div
-    className='grid grid-cols-2 gap-2 [&>button>svg]:size-[1.5em]
-[&>button>svg]:shrink-0'
-  >
-    <Button variant='outline' onClick={onGoogleClick}>
-      <GoogleLogo />
-    </Button>
-    <Button variant='outline' onClick={onGithubClick}>
-      <GithubLogo />
-    </Button>
+  <div>
+    <div className='mb-2 text-xs text-muted-foreground'>Sign in with</div>
+    <div
+      className='grid grid-cols-2 gap-2 [&>button>svg]:size-4
+[&>button>svg]:shrink-0 [&>button]:gap-2'
+    >
+      <Button
+        {...props}
+        type='button'
+        variant='outline'
+        onClick={onGoogleClick}
+      >
+        <GoogleLogo />
+        <span>Google</span>
+      </Button>
+      <Button
+        {...props}
+        type='button'
+        variant='outline'
+        onClick={onGithubClick}
+      >
+        <GithubLogo />
+        <span>GitHub</span>
+      </Button>
+    </div>
   </div>
 );
