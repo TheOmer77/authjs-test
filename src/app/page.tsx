e6489f44bba63759c581b01cb47f75051dd5ae81
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-17 13:29:40 +0200

    Add Logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { Logo } from '@/components/layout/Logo';
+
 const Home = () => (
   <main
     className='flex h-full flex-col items-center justify-center bg-background
 bg-[radial-gradient(circle_at_top,var(--tw-gradient-stops))] from-primary/30
 to-primary dark:from-primary/70 dark:to-primary/10'
   >
-    Hello world.
+    <div className='space-6'>
+      <Logo />
+    </div>
   </main>
 );
 
---
+++ src/components/layout/Logo.tsx
@@ -0,0 +1,15 @@
+import { LockClosedIcon } from '@radix-ui/react-icons';
+
+export const Logo = () => (
+  <h1
+    className='flex select-none flex-row items-center text-6xl
+font-extrabold tracking-tight text-white drop-shadow-md'
+  >
+    <LockClosedIcon className='size-48' />
+    <div className='flex flex-col'>
+      <span>The</span>
+      <span>Auth</span>
+      <span>Thing</span>
+    </div>
+  </h1>
+);
