96818b5294826ae3c00b8e559d7150109df0190f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-18 00:44:39 +0200

    Update settings page

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -3,7 +3,12 @@ import { auth } from '@/lib/auth';
 const SettingsPage = async () => {
   const session = await auth();
 
-  return <pre>{JSON.stringify(session, undefined, 2)}</pre>;
+  return (
+    <div className='flex h-full flex-col items-center justify-center gap-4'>
+      <p>Settings page TBD. In the meantime, here&apos;s your session info:</p>
+      <pre>{JSON.stringify(session, undefined, 2)}</pre>
+    </div>
+  );
 };
 
 export default SettingsPage;
