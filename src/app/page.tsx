630a7f63f6540333e5a799a7d9ac8ce41b9cddfb
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-17 14:32:42 +0200

    Move gradient background to body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ import { Logo } from '@/components/layout/Logo';
 import { SignInButton } from '@/components/auth/SignInButton';
 
 const Home = () => (
-  <main
-    className='flex h-full flex-col items-center justify-center bg-background
-bg-[radial-gradient(circle_at_top,var(--tw-gradient-stops))] from-primary/30
-to-primary dark:from-primary/70 dark:to-primary/10'
-  >
+  <main className='flex h-full flex-col items-center justify-center'>
     <div className='space-y-6 text-center'>
       <Logo />
       <SignInButton>
