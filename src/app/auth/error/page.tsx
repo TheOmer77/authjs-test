45bc99a8b01d4b2f8e1bc6302c78e0196843a22e
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-18 16:48:43 +0200

    Add auth error page

---
+++ src/app/auth/error/page.tsx
@@ -0,0 +1,12 @@
+import { CardDescription, CardHeader, CardTitle } from '@/components/ui/Card';
+
+const AuthErrorPage = () => (
+  <>
+    <CardHeader>
+      <CardTitle>Couldn&apos;t sign you in</CardTitle>
+      <CardDescription>Error description goes here... TBD</CardDescription>
+    </CardHeader>
+  </>
+);
+
+export default AuthErrorPage;
--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,5 +1,5 @@
 export const publicRoutes = ['/'];
-export const authRoutes = ['/auth/sign-in', '/auth/sign-up'];
+export const authRoutes = ['/auth/sign-in', '/auth/sign-up', '/auth/error'];
 
 export const API_AUTH_PREFIX = '/api/auth';
 export const DEFAULT_SIGNIN_REDIRECT = '/settings';
