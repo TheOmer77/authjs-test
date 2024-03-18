7cb1f0e9bc7759678895ae0d9df7b11f05ff624f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-18 14:25:11 +0200

    Add jwt and session auth callbacks

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,16 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import type { UserRole } from '@prisma/client';
 
 import { db } from './db';
+import { getUser } from '@/db/user';
 import authConfig from '@/config/auth';
+
+declare module 'next-auth' {
+  interface User {
+    role: UserRole;
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -13,4 +21,21 @@ export const {
   ...authConfig,
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
+  callbacks: {
+    jwt: async ({ token }) => {
+      if (!token.sub) return token;
+
+      const existingUser = await getUser({ id: token.sub });
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    },
+    session: async ({ token, session }) => {
+      if (token.sub && session.user) session.user.id = token.sub;
+      if (token.role && session.user)
+        session.user.role = token.role as UserRole;
+      return session;
+    },
+  },
 });
