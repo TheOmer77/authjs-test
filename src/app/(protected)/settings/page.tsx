1c45c66c11eec10c24b9de9ea58c5b333e4fbd07
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-20 13:15:19 +0200

    Update temporary settings page to look a little better

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/Button';
+import { Card, CardContent } from '@/components/ui/Card';
 import { signOut } from '@/actions/signOut';
 import { auth } from '@/lib/auth';
 
@@ -6,12 +7,18 @@ const SettingsPage = async () => {
   const session = await auth();
 
   return (
-    <div className='flex h-full flex-col items-center justify-center gap-4'>
-      <p>Settings page TBD. In the meantime, here&apos;s your session info:</p>
-      <pre>{JSON.stringify(session, undefined, 2)}</pre>
-      <form action={signOut}>
-        <Button type='submit'>Sign out</Button>
-      </form>
+    <div className='flex h-full items-center justify-center'>
+      <Card>
+        <CardContent className='flex flex-col gap-4 pt-6'>
+          <p>
+            Settings page TBD. In the meantime, here&apos;s your session info:
+          </p>
+          <pre>{JSON.stringify(session, undefined, 2)}</pre>
+          <form action={signOut} className='self-end'>
+            <Button type='submit'>Sign out</Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 };
