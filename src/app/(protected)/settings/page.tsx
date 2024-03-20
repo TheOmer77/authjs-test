c14087c5f9aa41a5ef1f5ae5ab6d0a0cbf27a710
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-20 20:40:53 +0200

    Improve temporary settings page layout

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Fragment } from 'react';
+
 import { Button } from '@/components/ui/Button';
 import { Card, CardContent } from '@/components/ui/Card';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
@@ -12,7 +14,18 @@ const SettingsPage = () => {
     <Card>
       <CardContent className='flex flex-col gap-4 pt-6'>
         <p>Settings page TBD. In the meantime, here&apos;s your user info:</p>
-        <pre>{JSON.stringify(user, undefined, 2)}</pre>
+        <div className='grid grid-cols-[auto,1fr] gap-x-4 gap-y-2'>
+          {user
+            ? Object.entries(user).map(([key, value]) => (
+                <Fragment key={key}>
+                  <span className='text-sm font-medium'>
+                    {`${key[0].toUpperCase()}${key.slice(1)}:`}
+                  </span>
+                  <span className='text-sm text-muted-foreground'>{`${value}`}</span>
+                </Fragment>
+              ))
+            : 'No user! 😱'}
+        </div>
         <Button className='self-end' onClick={() => signOut()}>
           Sign out
         </Button>
