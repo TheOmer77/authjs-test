e053c8fd15586a667c2bbdbd958404df0a67145e
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-20 18:20:52 +0200

    Use `useCurrentUser` in settings page

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,21 +1,21 @@
+'use client';
+
 import { Button } from '@/components/ui/Button';
 import { Card, CardContent } from '@/components/ui/Card';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
 import { signOut } from '@/actions/signOut';
-import { auth } from '@/lib/auth';
 
-const SettingsPage = async () => {
-  const session = await auth();
+const SettingsPage = () => {
+  const user = useCurrentUser();
 
   return (
     <Card>
       <CardContent className='flex flex-col gap-4 pt-6'>
-        <p>
-          Settings page TBD. In the meantime, here&apos;s your session info:
-        </p>
-        <pre>{JSON.stringify(session, undefined, 2)}</pre>
-        <form action={signOut} className='self-end'>
-          <Button type='submit'>Sign out</Button>
-        </form>
+        <p>Settings page TBD. In the meantime, here&apos;s your user info:</p>
+        <pre>{JSON.stringify(user, undefined, 2)}</pre>
+        <Button className='self-end' onClick={() => signOut()}>
+          Sign out
+        </Button>
       </CardContent>
     </Card>
   );
