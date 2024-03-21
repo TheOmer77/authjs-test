568c9b24e96b08bc65be4c3e42745216979ed399
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-21 13:44:40 +0200

    Update UserInfo values display names & formats

--- src/components/layout/UserInfo/UserInfo.tsx
+++ src/components/layout/UserInfo/UserInfo.tsx
@@ -1,6 +1,15 @@
 import { Fragment } from 'react';
 import type { User } from '@prisma/client';
 
+const displayedProperties = {
+  id: 'ID',
+  name: 'Name',
+  email: 'Email',
+  role: 'Role',
+  image: 'Image',
+  twofactor_enabled: '2FA enabled',
+} as const satisfies Partial<Record<keyof User, string>>;
+
 type UserInfoProps = {
   user?: Partial<User>;
 };
@@ -8,14 +17,34 @@ type UserInfoProps = {
 export const UserInfo = ({ user }: UserInfoProps) => (
   <div className='grid grid-cols-[auto,1fr] gap-x-4 gap-y-2'>
     {user
-      ? Object.entries(user).map(([key, value]) => (
-          <Fragment key={key}>
-            <span className='text-sm font-medium'>
-              {`${key[0].toUpperCase()}${key.slice(1)}:`}
-            </span>
-            <span className='text-sm text-muted-foreground'>{`${value}`}</span>
-          </Fragment>
-        ))
+      ? Object.entries(displayedProperties).map(([key, displayName]) => {
+          const value = user[key as keyof User];
+
+          let displayValue: string;
+          switch (true) {
+            case value instanceof Date:
+              displayValue = value.toISOString();
+              break;
+            case typeof value === 'boolean':
+              displayValue = `${value.toString()[0].toUpperCase()}${value.toString().slice(1)}`;
+              break;
+            case value === null || typeof value === 'undefined':
+              displayValue = 'None';
+              break;
+            default:
+              displayValue = value;
+              break;
+          }
+
+          return (
+            <Fragment key={key}>
+              <span className='text-sm font-medium'>{`${displayName}:`}</span>
+              <span className='truncate text-sm text-muted-foreground'>
+                {displayValue}
+              </span>
+            </Fragment>
+          );
+        })
       : 'No user! 😱'}
   </div>
 );
