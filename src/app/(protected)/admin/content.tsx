405bbed71ed78a0b93e12380682a5e2048d3f370
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-21 16:00:53 +0200

    Test admin API route & server action in admin page

--- src/app/(protected)/admin/content.tsx
+++ src/app/(protected)/admin/content.tsx
@@ -1,10 +1,35 @@
 'use client';
 
+import { useTransition } from 'react';
+
 import { Button } from '@/components/ui/Button';
 import { Card } from '@/components/ui/Card';
 import { RoleGate } from '@/components/auth/RoleGate';
+import { useToast } from '@/hooks/useToast';
+import { admin } from '@/actions/admin';
 
 const AdminPageContent = () => {
+  const { displayToast } = useToast();
+  const [isPending, startTransition] = useTransition();
+
+  const testApiRoute = () => {
+    startTransition(async () => {
+      const res = await (await fetch('/api/admin')).json();
+      displayToast(res.success ? 'API route works!' : res.error, {
+        variant: res.success ? 'default' : 'destructive',
+      });
+    });
+  };
+
+  const testServerAction = () => {
+    startTransition(async () => {
+      const res = await admin();
+      displayToast(res.success ? 'Server action works!' : res.error, {
+        variant: res.success ? 'default' : 'destructive',
+      });
+    });
+  };
+
   return (
     <>
       <RoleGate allowedRole='ADMIN'>
@@ -15,11 +40,15 @@ const AdminPageContent = () => {
       <Card>
         <div className='flex flex-row items-center justify-between px-4 py-2 text-sm'>
           Admin only API route
-          <Button>Test</Button>
+          <Button onClick={testApiRoute} disabled={isPending}>
+            Test
+          </Button>
         </div>
         <div className='flex flex-row items-center justify-between px-4 py-2 text-sm'>
           Admin only server action
-          <Button>Test</Button>
+          <Button onClick={testServerAction} disabled={isPending}>
+            Test
+          </Button>
         </div>
       </Card>
     </>
