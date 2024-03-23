36f3665a7051720291f32677cee897f4f677bb19
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-23 16:50:48 +0200

    Move SessionProvider to main layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,23 +1,16 @@
 import type { PropsWithChildren } from 'react';
-import { SessionProvider } from 'next-auth/react';
 
 import { Navbar } from './navbar';
 import { Card } from '@/components/ui/Card';
-import { auth } from '@/lib/auth';
 
-const ProtectedLayout = async ({ children }: PropsWithChildren) => {
-  const session = await auth();
-  return (
-    <SessionProvider session={session}>
-      <div
-        className='flex h-full flex-col items-center justify-center gap-1
+const ProtectedLayout = ({ children }: PropsWithChildren) => (
+  <div
+    className='flex h-full flex-col items-center justify-center gap-1
 p-4'
-      >
-        <Navbar />
-        <Card className='w-full max-w-md'>{children}</Card>
-      </div>
-    </SessionProvider>
-  );
-};
+  >
+    <Navbar />
+    <Card className='w-full max-w-md'>{children}</Card>
+  </div>
+);
 
 export default ProtectedLayout;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { PropsWithChildren } from 'react';
 import type { Metadata } from 'next';
 import { Manrope } from 'next/font/google';
+import { SessionProvider } from 'next-auth/react';
 
 import { Toaster } from '@/components/ui/Toast';
+import { auth } from '@/lib/auth';
 import '@/styles/index.css';
 
 const font = Manrope({ subsets: ['latin'], variable: '--font-sans' });
@@ -11,13 +13,18 @@ export const metadata: Metadata = {
   title: { template: '%s - The Auth Thing', default: 'The Auth Thing' },
 };
 
-const RootLayout = ({ children }: PropsWithChildren) => (
-  <html lang='en' className={font.variable}>
-    <body>
-      <Toaster />
-      {children}
-    </body>
-  </html>
-);
+const RootLayout = async ({ children }: PropsWithChildren) => {
+  const session = await auth();
+  return (
+    <SessionProvider session={session}>
+      <html lang='en' className={font.variable}>
+        <body>
+          <Toaster />
+          {children}
+        </body>
+      </html>
+    </SessionProvider>
+  );
+};
 
 export default RootLayout;
