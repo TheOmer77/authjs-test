fffeb2f00892a6d24296ec44783146ffd9b9d257
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-23 16:04:00 +0200

    Add Shadcn DIalog

---
+++ src/components/ui/Dialog/DialogHeader.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import type { ComponentPropsWithoutRef } from 'react';
+
+import { cn } from '@/lib/utils';
+
+export const DialogHeader = ({
+  className,
+  ...props
+}: ComponentPropsWithoutRef<'div'>) => (
+  <div
+    className={cn(
+      'flex flex-col space-y-1.5 text-center sm:text-left',
+      className
+    )}
+    {...props}
+  />
+);
+DialogHeader.displayName = 'DialogHeader';
